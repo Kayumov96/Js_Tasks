a1e9ee4cd62cf4057440fbd94dbc76b7b1e9bb6a
ArabboyKayumov <arabboykayumov@gmail.com>
2024-03-13 23:45:40 -0700

    function adding

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button");
+const box = document.getElementById("box");
+
+const color = document.getElementById("color");
+const form = document.getElementById("form");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
