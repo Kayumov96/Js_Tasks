81e069ce5693f2093d1232e8d2a7a53ff1c29efd
ArabboyKayumov <arabboykayumov@gmail.com>
2024-03-13 23:53:53 -0700

    function failure

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const color = document.getElementById("color");
 const form = document.getElementById("form");
 const width = document.getElementById("width");
 const height = document.getElementById("height");
+
+button.addEventListener("click", () => {
+  if (color.value === "") return alert("Please select a color");
+
+  box.style = `
+  background-color: ${color.value};
+  width: ${width.value};
+  border-radius: ${form.value === "circle" ? "50%" : "0"}
+  height: ${height.value};
+  `;
+});
