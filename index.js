e2fb060047d0605db0f17d643304674bfc39f5b2
ArabboyKayumov <arabboykayumov@gmail.com>
2024-03-28 23:11:51 -0700

    error with creating div

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-// const button = document.getElementById("button");
-// const box = document.getElementById("box");
+const button = document.getElementById("button");
+const box = document.getElementById("result");
 
-// const color = document.getElementById("color");
-// const form = document.getElementById("form");
-// const width = document.getElementById("width");
-// const height = document.getElementById("height");
+const color = document.getElementById("color");
+const form = document.getElementById("form");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
 
-// button.addEventListener("click", () => {
-//   if (color.value === "") return alert("Please select a color");
+button.addEventListener("click", () => {
+  // if (color.value === "") return alert("Please select a color");
 
-//   box.style = `
-//   background-color: ${color}.value;
-//   width: ${width.value};
-//   border-radius: ${form.value === "circle" ? "50%" : "0"}
-//   height: ${height.value};
-//   `;
-// });
-// function createDiv() {
-//   const color = document.getElementById("color").value;
-//   const width = document.getElementById("width").value;
-//   const height = document.getElementById("height").value;
+  result.style = `
+  background-color: ${color}.value;
+  width: ${width.value}px;
+  height: ${height.value}px;
+  `;
+  if (form.value === "square") {
+    result?.style?.borderRadius = "0";
+  } else if (form.value === "circle") {
+    result?.style?.borderRadius = "50%";
+  }
+});
 
-//   const div = document.createElement("div");
-//   div.style.backgroundColor = color;
-//   div.style.width = width + "px";
-//   div.style.height = height + "px";
-
-//   document.getElementById("result").appendChild(div);
-// }
 
 // --------------------------------------------------------
 // ----------
 
-let arr = [1, 2, 5, 3, 1];
+// let arr = [1, 2, 5, 3, 1];
 
-function testPeaks(arr) {
-  let peak = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > peak) {
-      peak.push += i;
-    }
-  }
-  console.log(peak, peak[0]);
-}
-console.log(testPeaks(arr));
+// function testPeaks(arr) {
+//   let peak = [];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] > peak) {
+//       peak.push += i;
+//     }
+//   }
+//   console.log(peak, peak[0]);
+// }
+// console.log(testPeaks(arr));
+// -------------------
